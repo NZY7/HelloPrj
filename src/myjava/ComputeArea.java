356627f2321a3ac02bee2970d3a5278090d4125e
NZY7 <1610707371@qq.com>
2022-03-03 10:17:36 +0800

    java

---
+++ src/myjava/ComputeArea.java
@@ -0,0 +1,11 @@
+package myjava;
+
+public class ComputeArea {
+	public static void main(String[] agrs) {
+		double radius;
+		double area;
+		radius=20;
+		area=radius*radius*3.14159;
+		System.out.println("THe area of circle of radius "+radius+" is "+area);
+	}
+}
